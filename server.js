a5e61ca88859fadbd96dd95d2ec498d552fbfeb5
unknown <ahshawngoh@gmail.com>
2022-04-18 20:32:20 +0800

    fix

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : process.env.DATABASE_URL,
+      connectionString : process.env.DATABASE_URL,
       ssl: true
       // port : 5432,
       // user : 'postgres',
